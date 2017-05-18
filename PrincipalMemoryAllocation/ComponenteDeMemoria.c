3421acc74fc2f212744c9edfa3fd6cdec59806d0
Luis Diniz <dinnizluis@gmail.com>
2017-05-18 09:59:42 -0300

    Alocação First Fit funcionando parcialmente (encadeamento duplo precisa ser corrigido assim como função geradora de pids)

--- PrincipalMemoryAllocation/ComponenteDeMemoria.c
+++ PrincipalMemoryAllocation/ComponenteDeMemoria.c
@@ -14,15 +14,18 @@ void init_segment(Segment * seg, int begin, int length, int status, int pid){
 }
 
 void init(ComponenteDeMemoria * mem){
+	Segment seg;
+	init_segment(&seg, 0, 256, L, -1);
 	mem->free = 256;
+	
 	mem->first = (CellPointer) malloc(sizeof(MemCell));
 	mem->first->next = (CellPointer) malloc(sizeof(MemCell));
 	
-	Segment seg;
-	init_segment(&seg, 0, 256, L, -1);
 	mem->first->next->seg = seg;
 	mem->first->prev = NULL;
-	mem->last = mem->first;
+
+	mem->last->prev = mem->first->next;
+	mem->last->next = mem->first->prev;
 }
 
 int full_free(ComponenteDeMemoria mem){
@@ -32,7 +35,7 @@ int full_free(ComponenteDeMemoria mem){
 void show(ComponenteDeMemoria mem){
 	CellPointer aux;
 	aux = mem.first->next;
-	//aux = mem.last;
+	//aux = mem.last->prev;
 	printf("====================== Segment Information ======================\n");
 	while(aux != NULL){
 		if(aux->seg.status){//Verifica se a estrutura Process foi inicializada
@@ -47,52 +50,62 @@ void show(ComponenteDeMemoria mem){
 			printf("length: %d\n", aux->seg.length);
 		}
 		aux = aux->next;
+		//aux = aux->prev;
 		printf("\n");
 	}
-	printf("Free space: %dkB\n", mem.free);
+	printf("Memory available: %dkB\n", mem.free);
 }
 
-// ===================== Allocation Methods ===================== //
+// ======================== Allocation Methods ======================== //
 int allocate_mem_ff(int pid, int num_units, ComponenteDeMemoria * mem){
 	CellPointer aux, new;
-	int addr = 0;
-	int num_cell = 0;
+	int addr, num_cell = 0;
 	aux = mem->first->next;
+
 	while(aux != NULL){
 		num_cell++;
 		if(!aux->seg.status){//Verifica se o segmento está livre
+			
+			//Recebe endereço inicial do segmento
+			if(aux->prev != NULL)
+				addr = aux->prev->seg.begin + aux->prev->seg.length;
+			else
+				addr = 0;
+			
 			if(num_units*2 == aux->seg.length){
+				//Criando segmento para armazenar o processo
 				Segment s;
-				init_segment(&s, addr, num_units*2, P, pid);//Criando segmento para armazenar o processo
+				init_segment(&s, addr, num_units*2, P, pid);
+
+				//Atualiza dados do segmento, total de memória livre e endereço para próximo segmento
 				aux->seg = s;
 				mem->free = mem->free - num_units*2;
 				addr = addr + num_units*2;
 				return num_cell;
 			}
 			else if(num_units*2 < aux->seg.length){
+				//Criando segmento para armazenar o processo
 				Segment s;
-				init_segment(&s, addr, num_units*2, P, pid);//Criando segmento para armazenar o processo
-				addr = addr + num_units*2;
-				/*if(aux->next != NULL){//Caso em que o segmento livre não é o último
-					
-					aux->next->seg.length = aux->seg.length - num_units*2;
-					aux->next->seg.begin = addr;
-					aux->seg = s;
-					
-				}
-				else{//Caso em que o segmento livre é o último
-				*/	//Criando novo segmento livre (resultado da divisão)
-					Segment s2; 
-					init_segment(&s2, addr, (aux->seg.length - num_units*2), L, pid); 
-					new = (CellPointer) malloc(sizeof(MemCell));
+				init_segment(&s, addr, num_units*2, P, pid);
 
-					//Atualizando apontadores
-					new->next = aux->next;
-					new->prev = aux;
-					aux->next = new;
-					aux->seg = s;
-					aux->next->seg = s2;
-				//}
+				//Atualiza endereço para o próximo segmento
+				addr = addr + num_units*2;
+
+				//Cria novo segmento livre (resultado da divisão do bloco inicialmente livre)
+				Segment new_seg; 
+				init_segment(&new_seg, addr, (aux->seg.length - num_units*2), L, pid); 
+				new = (CellPointer) malloc(sizeof(MemCell));
+
+				//Atualizando apontadores
+				new->next = aux->next;
+				new->prev = aux;
+				//new->next->prev = new;
+				//aux->next->prev = new;
+				aux->next = new;
+				aux->seg = s;
+				aux->next->seg = new_seg;
+				
+				//Atualiza total de de memória livre
 				mem->free = mem->free - num_units*2;
 				return num_cell;
 			}
--- PrincipalMemoryAllocation/main.c
+++ PrincipalMemoryAllocation/main.c
@@ -11,14 +11,18 @@ void generate_processes(int n, long int * pids){//n é o número de processos
 }
 
 int main(){
-	long int * pids;
-	generate_processes(5, pids);
-	Segment seg;
+	//Declaração de variáveis
 	ComponenteDeMemoria mem;
+	long int * pids;
+	
+	//Geração de pids
+	//generate_processes(5, pids);
+	
 	init(&mem);
-	allocate_mem_ff(pids[0], 10, &mem);
-	allocate_mem_ff(pids[1], 3, &mem);
-	/*allocate_mem_ff(pids[2], 5, &mem);
+	printf("%d\n", full_free(mem));
+	allocate_mem_ff(333, 10, &mem);
+	/*allocate_mem_ff(pids[1], 3, &mem);
+	allocate_mem_ff(pids[2], 5, &mem);
 	allocate_mem_ff(pids[3], 7, &mem);
 	allocate_mem_ff(pids[4], 4, &mem);*/
 	show(mem);
