9fdba673a3cdcf456c697efd0ce3d5b3dad5a6d0
Luis Diniz <dinnizluis@gmail.com>
2017-05-30 17:30:42 -0300

    Gerador de requisições atualizado e precisa de correções

--- PrincipalMemoryAllocation/ComponenteDeRequisicao.c
+++ PrincipalMemoryAllocation/ComponenteDeRequisicao.c
@@ -3,77 +3,88 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*int allocate_requisition(ComponenteDeMemoria  *mem, int option){
-	int pid,num_units, c;
-	pid = 1 + (rand()%300);
-	num_units= 3 + (rand()%10);
-	switch(option);
-	case 1:
-		c = allocate_mem_ff(pid,num_units, &(*mem));
-		if(c == -1)
-			return -1;
-		else 
-			return c;
-	case 2:
-		c = allocate_mem_wf(pid,num_units, &(*mem));
-		if(c == -1)
-			return -1;
-		else 
-			return c;
 
-}
+void requistions_generator(int num_req, int size){ 
+	ComponenteDeMemoria mem_bf, mem_nf, mem_wf, mem_ff;
+	
+	init(&mem_wf, size);
+	init(&mem_ff, size);
+	init(&mem_nf, size);
+	init(&mem_bf, size);
 
-int deallocate_requisition(ComponenteDeMemoria  *mem){
-	int pid;
-	pid = 1 + (rand()%300);
-	deallocate_mem(pid,&(*mem));
-}*/
-
-void requistions_generator(int num_req, ComponenteDeMemoria  *memff,ComponenteDeMemoria  *memwf){ 
 	int i,x,allocation_attempt=0;
 	int pid, num_units;
-	int errorff=0, sumff =0, ff;
-	int errorwf=0, sumwf=0, wf;
+	int errorff = 0, sumff = 0, ff;
+	int errorwf = 0, sumwf = 0, wf;
+	int errornf = 0, sumnf = 0, nf;
+	int errorbf = 0, sumbf = 0, bf;
 	struct timeval semente;
+
     /* Utilizar o tempo como semente para a funcao srand() */
     gettimeofday(&semente, NULL); 
+
     srand((int)(semente.tv_sec + 1000000 * semente.tv_usec));
-  	for(i=0; i<num_req; i++){
+  	
+  	for(i = 0; i < num_req; i++){
 		x = (rand()%2);
 		if(x == 0){
 			pid = 1 + (rand()%300);
 			num_units= 3 + (rand()%10);
 			allocation_attempt++;
-			ff = allocate_mem_ff(pid,num_units, &(*memff));
-			wf = allocate_mem_wf(pid,num_units, &(*memwf));
+
+			ff = allocate_mem_ff(pid,num_units, &mem_ff);
+			wf = allocate_mem_wf(pid,num_units, &mem_wf);
+			nf = allocate_mem_nf(pid,num_units, &mem_nf);
+			bf = allocate_mem_bf(pid,num_units, &mem_bf);
+
 			// Verificação First Fit
 			if(ff == -1)
 				errorff++;
 			else 
 				sumff = sumff + ff;
+
 			// Verificação Worst Fit
 			if(wf == -1)
 				errorwf++;
 			else 
 				sumwf = sumwf + wf;
-		}
 
+			// Verificação Next Fit
+			if(nf == -1)
+				errornf++;
+			else 
+				sumnf = sumnf + nf;
+
+			// Verificação Best Fit
+			if(bf == -1)
+				errorbf++;
+			else 
+				sumbf = sumbf + bf;
+		}
 		else{
 			pid = 1 + (rand()%300);
-			deallocate_mem(pid, &(*memff));
-			deallocate_mem(pid, &(*memwf));
-
-		
+			deallocate_mem(pid, &mem_ff);
+			deallocate_mem(pid, &mem_wf);
+			deallocate_mem(pid, &mem_nf);
+			deallocate_mem(pid, &mem_bf);
 		}
 
 	}
-	/*printf("Total de Erros: %d\n",error );
+
+	printf("ESTATÍSTICAS:\n\n");
 	printf("Tentativas de Alocacao: %d\n", allocation_attempt );
-	printf("Total de celulas percorridas: %d\n", sum );*/
-	printf("Tentativas de Alocacao: %d\n", allocation_attempt );
-	printf("=================First Fit====================\n");
-	statistics(*memff, errorff, sumff, allocation_attempt);
-	printf("=================Worst Fit====================\n");
-	statistics(*memwf, errorwf, sumwf, allocation_attempt);
-	
+	printf("==========================First Fit==============================\n");
+	statistics(mem_ff, errorff, sumff, allocation_attempt);
+	printf("\n");
+	printf("==========================Worst Fit==============================\n");
+	statistics(mem_wf, errorwf, sumwf, allocation_attempt);
+	printf("\n");
+	printf("==========================Next Fit==============================\n");
+	statistics(mem_nf, errornf, sumnf, allocation_attempt);
+	printf("\n");
+	printf("==========================Best Fit==============================\n");
+	statistics(mem_bf, errorbf, sumbf, allocation_attempt);
+	printf("\n");
+	printf("-----------------------------------------------------------------\n");
+
 }
--- PrincipalMemoryAllocation/main.c
+++ PrincipalMemoryAllocation/main.c
@@ -3,56 +3,11 @@
 #include <time.h>
 #include "ComponenteDeMemoria.h"
 
-void generate_processes(int n, long int * pids){//n é o número de processos
-	int i;
-	for(i = 0; i < n; i++){
-		pids[i] = time(NULL)+i;
-	}
-}
+
 
 int main(){
-	//Declaração de variáveis
-	ComponenteDeMemoria mem, mem_wf, mem_ff;
-	long int * pids;
 	
+	requistions_generator(100, 256);
 
-	//printf("=============First Fit==========\n");	
-	printf("Memory length: 256\n");
-	init(&mem_ff, 256);
-	init(&mem_wf, 256);
-	init(&mem, 256);
-	int i = 3;
-	while(i < 11){
-		allocate_mem_ff(100+i, i, &mem);
-		i++;
-	}
-	deallocate_mem(107, &mem);
-	deallocate_mem(104, &mem);
-	deallocate_mem(109, &mem);
-	show(mem);
-	printf("\n\n\n");
-	allocate_mem_nf(666, 7, &mem);
-	allocate_mem_nf(777, 4, &mem);
-	show(mem);
-	printf("%d\n", mem.search->seg.pid);
-	//requistions_generator(10000, &mem_ff, &mem_wf);
-	//show(mem);
-	/*
-	//show(mem);
-	file_write(mem);
-	printf("Memory length: 512\n");
-	init(&mem, 512);
-	requistions_generator(10000, &mem);
-	//show(mem);
-	file_write(mem);
-	printf("Memory length: 1024\n");
-	init(&mem, 1024);
-	requistions_generator(10000, &mem);
-	show(mem);
-	file_write(mem);
-	
-	file_write(mem_wf);
-	file_write(mem_ff);
-	*/
 	return 0;
 }
